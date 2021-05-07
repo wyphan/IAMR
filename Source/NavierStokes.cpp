017288189fa17bed9e76205c0f93c3d5c0a761ed
Michele Rosso <mrosso@lbl.gov>
2021-05-07 16:42:03 -0700

    Another attenmpt

--- Source/NavierStokes.cpp
+++ Source/NavierStokes.cpp
@@ -792,22 +792,22 @@ NavierStokes::scalar_advection (Real dt,
             // MultiFab::Saxpy(*divu_fp, 0.5*dt, *dsdt, 0, 0, 1, nghost_force());
             // delete dsdt;
 
-#ifdef AMREX_USE_EB
-            EBMOL::ComputeAofs(*aofs, fscalar, num_scalars, Smf, 0,
-                               D_DECL(u_mac[0],u_mac[1],u_mac[2]),
-                               D_DECL(edgestate[0],edgestate[1],edgestate[2]), 0, false,
-                               D_DECL(cfluxes[0],cfluxes[1],cfluxes[2]), 0,
-                               *divu_fp,
-                               m_bcrec_scalars, m_bcrec_scalars_d.dataPtr(), iconserv,
-                               geom, dt, false, redistribution_type );
-#else
-            MOL::ComputeAofs(*aofs, fscalar, num_scalars, Smf, 0,
-                             D_DECL(u_mac[0],u_mac[1],u_mac[2]),
-                             D_DECL(edgestate[0],edgestate[1],edgestate[2]), 0, false,
-                             D_DECL(cfluxes[0],cfluxes[1],cfluxes[2]), 0,
-                             m_bcrec_scalars, m_bcrec_scalars_d.dataPtr(), iconserv,
-                             geom, false );
-#endif
+// #ifdef AMREX_USE_EB
+//             EBMOL::ComputeAofs(*aofs, fscalar, num_scalars, Smf, 0,
+//                                D_DECL(u_mac[0],u_mac[1],u_mac[2]),
+//                                D_DECL(edgestate[0],edgestate[1],edgestate[2]), 0, false,
+//                                D_DECL(cfluxes[0],cfluxes[1],cfluxes[2]), 0,
+//                                *divu_fp,
+//                                m_bcrec_scalars, m_bcrec_scalars_d.dataPtr(), iconserv,
+//                                geom, dt, false, redistribution_type );
+// #else
+//             MOL::ComputeAofs(*aofs, fscalar, num_scalars, Smf, 0,
+//                              D_DECL(u_mac[0],u_mac[1],u_mac[2]),
+//                              D_DECL(edgestate[0],edgestate[1],edgestate[2]), 0, false,
+//                              D_DECL(cfluxes[0],cfluxes[1],cfluxes[2]), 0,
+//                              m_bcrec_scalars, m_bcrec_scalars_d.dataPtr(), iconserv,
+//                              geom, false );
+// #endif
 
         }
         else
@@ -876,18 +876,18 @@ NavierStokes::scalar_advection (Real dt,
             }
 
 
-#ifdef AMREX_USE_EB
-            EBGodunov::ComputeAofs(*aofs, fscalar, num_scalars,
-                                   Smf, 0,
-                                   AMREX_D_DECL(u_mac[0],u_mac[1],u_mac[2]),
-                                   AMREX_D_DECL(edgestate[0],edgestate[1],edgestate[2]), 0, false,
-                                   AMREX_D_DECL(cfluxes[0],cfluxes[1],cfluxes[2]), 0,
-                                   forcing_term, 0, *divu_fp,
-                                   m_bcrec_scalars, m_bcrec_scalars_d.dataPtr(),
-                                   geom, iconserv, dt, true, redistribution_type );
+// #ifdef AMREX_USE_EB
+//             EBGodunov::ComputeAofs(*aofs, fscalar, num_scalars,
+//                                    Smf, 0,
+//                                    AMREX_D_DECL(u_mac[0],u_mac[1],u_mac[2]),
+//                                    AMREX_D_DECL(edgestate[0],edgestate[1],edgestate[2]), 0, false,
+//                                    AMREX_D_DECL(cfluxes[0],cfluxes[1],cfluxes[2]), 0,
+//                                    forcing_term, 0, *divu_fp,
+//                                    m_bcrec_scalars, m_bcrec_scalars_d.dataPtr(),
+//                                    geom, iconserv, dt, true, redistribution_type );
 
 
-#else
+// #else
             Godunov::ComputeAofs(*aofs, fscalar, num_scalars,
                                  Smf, 0,
                                  AMREX_D_DECL( u_mac[0], u_mac[1], u_mac[2] ),
@@ -895,7 +895,7 @@ NavierStokes::scalar_advection (Real dt,
                                  AMREX_D_DECL( cfluxes[0], cfluxes[1], cfluxes[2] ), 0,
                                  forcing_term, 0, *divu_fp, m_bcrec_scalars_d.dataPtr(),
                                  geom, iconserv, dt, godunov_use_ppm, godunov_use_forces_in_trans, false );
-#endif
+// #endif
 
         }
 
